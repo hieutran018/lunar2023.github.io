f945040f35b4544fc101f0c4df4805d69e2b5fda
hieutran018 <73825993+hieutran018@users.noreply.github.com>
2023-01-21 00:59:28 +0700

    fix audio

--- script.js
+++ script.js
@@ -2097,7 +2097,7 @@ const Spark = {
 
 
 const soundManager = {
-	baseURL: 'https://*s3-us-west-2.amazonaws.com/s.cdpn.io/329180/',
+	baseURL: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/329180/',
 	ctx: new (window.AudioContext || window.webkitAudioContext),
 	sources: {
 		lift: {
